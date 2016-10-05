cfb579ab628880b11de1c020da091e05ac31b355
1-718-8 <1-718-8@24-717-8-PC>
2016-10-05 13:42:05 +0900

    ApplicationConfig 작성

--- leaseCity/src/main/java/leasecity/config/ApplicationConfig.java
+++ leaseCity/src/main/java/leasecity/config/ApplicationConfig.java
@@ -1,8 +1,21 @@
 package leasecity.config;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan(basePackages={
@@ -10,7 +23,61 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 		"leasecity.repo", 
 		"leasecity.config"
 		})
-@EnableWebMvc
+@PropertySource("classpath:/config/dbconfig.properties")
+@EnableTransactionManagement
 public class ApplicationConfig {
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource ds){
+		PlatformTransactionManager tm = new DataSourceTransactionManager(ds);
+		return tm;
+	}// 트랜잭션 매니저 설정
+	
+	@Bean
+	public DataSource dataSource(
+			@Value("${ds.driverClassName}") String driverClassName,
+			@Value("${ds.url}") String url,
+			@Value("${ds.userName}") String userName,
+			@Value("${ds.password}") String password,
+			@Value("${ds.maxTotal}") int maxTotal,
+			@Value("${ds.initialSzie}") int initialSize,
+			@Value("${ds.maxIdle}") int maxIdle){
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName(driverClassName);
+		ds.setUrl(url);
+		ds.setUsername(userName);
+		ds.setPassword(password);
+		ds.setMaxTotal(maxTotal);
+		ds.setInitialSize(initialSize);
+		ds.setMaxIdle(maxIdle);
+		
+		return ds;
+	}// 데이터소스 연결
+	
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource ds){
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(ds);
+		String loc = "config/mybatis-config.xml";
+		bean.setConfigLocation(new ClassPathResource(loc));
+		
+		return bean;
+	}// mybatis연동
+	
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactoryBean sfb) throws Exception{
+		SqlSessionTemplate template = new SqlSessionTemplate(sfb.getObject());
+		
+		return template;
+	} // mybatis연동으로 SQLSession생성
+	
+	
+	@Bean
+	public MessageSource messageSource(){
+		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+		ms.setCacheSeconds(0);
+		ms.setBasename("classpath:/message/error/binding");
+		return ms;
+	}//error시 문구 작성
 
 }
--- leaseCity/src/test/java/leasecity/test/PsTest.java
+++ leaseCity/src/test/java/leasecity/test/PsTest.java
@@ -1,20 +1,28 @@
 package leasecity.test;
 
-import static org.junit.Assert.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import leasecity.config.ApplicationConfig;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={ApplicationConfig.class})
 public class PsTest {
 
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void passwordTest() throws NoSuchAlgorithmException {
+		
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		
+		
+		
 	}
 
 }
