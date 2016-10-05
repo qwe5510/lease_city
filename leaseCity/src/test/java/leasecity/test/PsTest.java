88af0130aae5addc2425d1e21bd54f96517a72fc
1-718-8 <1-718-8@24-717-8-PC>
2016-10-05 13:23:34 +0900

    DTO package 분류

---
+++ leaseCity/src/test/java/leasecity/test/PsTest.java
@@ -0,0 +1,20 @@
+package leasecity.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class PsTest {
+
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
