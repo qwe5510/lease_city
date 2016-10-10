42dba483a3239deb1b278c06c7a21d5aefda6067
1-718-8 <1-718-8@24-717-8-PC>
2016-10-10 18:39:50 +0900

    대기유저작업1

--- leaseCity/src/main/java/leasecity/service/StandByUserServiceImpl.java
+++ leaseCity/src/main/java/leasecity/service/StandByUserServiceImpl.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.exceptions.PersistenceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import leasecity.dto.adminwork.StandByUser;
 import leasecity.exception.DuplicateValueException;
@@ -13,6 +14,7 @@ import leasecity.exception.NotFoundDataException;
 import leasecity.repo.adminwork.StandByUserRepo;
 import leasecity.util.HashingUtil;
 
+@Service
 public class StandByUserServiceImpl implements StandByUserService {
 
 	@Autowired
