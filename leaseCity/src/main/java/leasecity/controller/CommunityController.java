7603929f51b183b873a5b503b69a6f2a80721a9f
1-718-8 <1-718-8@24-717-8-PC>
2016-10-18 15:36:12 +0900

    게시글 검색 및 페이지넘기기 끝

--- leaseCity/src/main/java/leasecity/controller/CommunityController.java
+++ leaseCity/src/main/java/leasecity/controller/CommunityController.java
@@ -1,14 +1,10 @@
 package leasecity.controller;
 
-import java.util.Collections;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,23 +44,35 @@ public class CommunityController {
 	
 	//커뮤니티 메인 페이지
 	@RequestMapping(value="/board", method=RequestMethod.GET)
-	public String board(Model model,
+	public String board(Model model, Page searchPage,
 			@RequestParam(value="currentPage", required=false) 
-			Integer currentPage){
+			Integer currentPage,
+			@RequestParam(value="order", required=false)
+			String order){
 	
+		Page page = null;
+		List<Comment> comments = null;
+		
 		//값이 없으면 1대입.
 		if(currentPage == null)
 			currentPage = 1;
-		Page page = communityService.getCommentPage(currentPage, 20);
-
 		try {
-			List<Comment> comments = communityService.loadPageCommentList(page);
-			model.addAttribute("comments", comments);
-			model.addAttribute("page", page);
-			//communityService.getSearchCommentPage(currentPage, pageSize, search, keyword, order)
+			if(searchPage != null){
+				page = communityService.getSearchCommentPage
+						(currentPage, PAGE_SIZE, searchPage.getSearch(),
+								searchPage.getKeyword(), order);
+				logger.trace("page : {}", page);
+				comments = communityService.loadTermsComment(page);
+			}else if(searchPage == null){
+				page = communityService.getCommentPage(currentPage, PAGE_SIZE);
+				comments = communityService.loadPageCommentList(page);
+			}			
+				model.addAttribute("comments", comments);
+				model.addAttribute("page", page);
 		} catch (NotFoundDataException e) {
 			logger.error("게시글이 없음");
-		} 				
+		}
+				
 		return "community/board";
 	}
 	
